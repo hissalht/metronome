f44376d6d0d29d6d61fef3bfe86dc252fd12b217
Adrien Boutigny <adrien.boutigny@gmail.com>
2018-10-03 00:23:27 +0200

    play metronome sound on play button click

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { Slider } from '@blueprintjs/core'
+import { Button, Slider } from '@blueprintjs/core'
+import { Howl } from 'howler'
 
 import styles from './App.module.sass'
 import Tapper from './Tapper'
 import TempoInput from './TempoInput'
+import clickUrl from '../sounds/metronome.ogg'
 
 export const MAX_BPM = 250
 export const MIN_BPM = 60
@@ -17,30 +19,71 @@ class App extends Component {
   state = {
     bpm: 120,
     sound: 'default',
+    playing: false,
   }
 
-  handleChange = name => value => this.setState({ [name]: value })
+  sound = new Howl({
+    src: [clickUrl],
+    volume: .5
+  })
 
-  handleSliderChange = value => this.setState({ bpm: Math.round(value) })
+  handleSliderChange = value => this.handleBpmChange(Math.round(value))
+
+  handleBpmChange = value => {
+    if (this.state.playing) {
+      this.setSoundInterval(value)
+    }
+    this.setState({ bpm: value })
+  }
+
+  handlePlayButtonClick = e => {
+    // TODO: Replace setInterval calls with a more precise scheduling method
+    if (this.state.playing) {
+      this.clearSoundInterval()
+    } else {
+      this.sound.play()
+      this.setSoundInterval(this.state.bpm)
+    }
+    this.setState(({ playing }) => ({ playing: !playing}))
+  }
+
+  setSoundInterval = bpm => {
+    clearInterval(this.soundHandle)
+    this.soundHandle = setInterval(
+      () => this.sound.play(),
+      60000/bpm
+    )
+  }
+
+  clearSoundInterval = () => {
+    clearInterval(this.soundHandle)
+  }
 
   render() {
-    const { bpm } = this.state
+    const { bpm, playing } = this.state
     return (
       <div className={styles.root}>
         <div className={styles.container}>
           <Tapper
             text='Tap here'
-            onChange={this.handleChange('bpm')}
+            onChange={this.handleBpmChange}
             min={MIN_BPM}
             max={MAX_BPM}
           />
           <br />
-          <TempoInput
-            value={bpm}
-            onChange={this.handleChange('bpm')}
-            min={MIN_BPM}
-            max={MAX_BPM}
-          />
+          <div className={styles.playLine}>
+            <TempoInput
+              value={bpm}
+              onChange={this.handleBpmChange}
+              min={MIN_BPM}
+              max={MAX_BPM}
+            />
+            <Button
+              icon={playing ? 'stop' : 'play'}
+              large
+              onClick={this.handlePlayButtonClick}
+            />
+          </div>
           <br />
           <Slider
             value={bpm}
